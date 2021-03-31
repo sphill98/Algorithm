a961d7d4eccb52cece9f5cae3ba46bc10666bd30
sphill98 <sphill9803@gmail.com>
2021-03-31 23:21:44 +0900

    Update bj11723.py

--- BJ/bj11723.py
+++ BJ/bj11723.py
@@ -5,20 +5,20 @@ s = [0]*20
 M = int(sys.stdin.readline())
 
 for i in range(M):
-  cmd = sys.stdin.readline().split()
-  if cmd[0] == 'add':
-    k = int(cmd[1])
-    s[k-1] = 1
-  elif cmd[0] == 'remove':
-    k = int(cmd[1])
-    s[k-1] = 0
-  elif cmd[0] == 'check':
-    k = int(cmd[1])
-    print (s[k-1])
-  elif cmd[0] == 'toggle':
-    k = int(cmd[1])
-    s[k-1] = s[k-1] ^ 1
-  elif cmd[0] == 'all':
+  ipt = sys.stdin.readline().split()
+  if len(ipt) == 1:
+    cmd = ipt[0]
+  else:
+    cmd, x = ipt[0], int(ipt[1])
+  if cmd == 'add':
+    s[x-1] = 1
+  elif cmd == 'remove':
+    s[x-1] = 0
+  elif cmd == 'check':
+    print (s[x-1])
+  elif cmd == 'toggle':
+    s[x-1] = s[x-1] ^ 1
+  elif cmd == 'all':
     s = [1]*20
-  elif cmd[0] == 'empty':
+  elif cmd == 'empty':
     s = [0]*20
