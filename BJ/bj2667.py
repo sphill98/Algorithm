a07ef73e31cda5fa80f83ce7cad326a790384348
sphill98 <sphill9803@gmail.com>
2023-11-20 16:45:48 +0900

    Update bj2667.py

--- BJ/bj2667.py
+++ BJ/bj2667.py
@@ -1,42 +1,45 @@
 from collections import deque
 
+#Variables
+
+Gr = []
+
+N = int(input())
+
+for i in range(N):
+    Gr.append(list(input()))
+
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
 
+lst = []
 
-def bfs(graph, a, b):
-    n = len(graph)
+def BFS(G, v):
+    cnt = 0
     queue = deque()
-    queue.append((a, b))
-    graph[a][b] = 0
-    count = 1
-
-    while queue:
-        x, y = queue.popleft()
+    G[v[0]][v[1]] = '0' # 0은 방문 1은 미방문
+    queue.append(v)
+    while queue: #queue가 빌 때까지 진행
+        w = queue.popleft() #가장 첫번째 꺼내서
+        cnt += 1
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0 or nx >= n or ny < 0 or ny >= n:
-                continue
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = 0
-                queue.append((nx, ny))
-                count += 1
-    return count
+            x = w[0] + dx[i] #위 아래 연결된 원소 찾기.
+            y = w[1] + dy[i]
+            if (x >= 0 and x < N) and (y >= 0 and y < N): #인덱스 에러 방지
+                if G[x][y] == '1':
+                    queue.append([x, y])
+                    G[x][y] = '0'
+    return G, cnt
 
 
-n = int(input())
-graph = []
-for i in range(n):
-    graph.append(list(map(int, input())))
+for i in range(N):
+    for j in range(N):
+        if Gr[i][j] == '1':
+            v = [i, j]
+            Gr, tmp = BFS(Gr, v)
+            lst.append(tmp)
 
-cnt = []
-for i in range(n):
-    for j in range(n):
-        if graph[i][j] == 1:
-            cnt.append(bfs(graph, i, j))
-
-cnt.sort()
-print(len(cnt))
-for i in range(len(cnt)):
-    print(cnt[i])
+lst.sort()
+print(len(lst))
+for s in lst:
+    print(s)
