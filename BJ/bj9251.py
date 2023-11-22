5f80602e0818b761aa4bb3de9798b5c0f8046a40
sphill98 <sphill9803@gmail.com>
2023-11-22 15:14:47 +0900

    Update bj9251.py

--- BJ/bj9251.py
+++ BJ/bj9251.py
@@ -1,9 +1,9 @@
 def LCS(A, B):
     na, nb = len(A), len(B)
-    LCS_table = [([0] * (nb + 1)) for _ in range(na + 1)]
-    for i in range(1, na + 1):
+    LCS_table = [([0] * (nb + 1)) for _ in range(na + 1)] #2차원 배열을 이용한 DP
+    for i in range(1, na + 1): 
         for j in range(1, nb + 1):
-            if A[i - 1] == B[j - 1]:
+            if A[i - 1] == B[j - 1]: #문자열 같으면 하나 증가시켜줌. 대각선으로 내려가면서 증가시켜서 문자열 찾는 방식.
                 LCS_table[i][j] = LCS_table[i - 1][j - 1] + 1
             else:
                 LCS_table[i][j] = max(LCS_table[i - 1][j], LCS_table[i][j - 1])
